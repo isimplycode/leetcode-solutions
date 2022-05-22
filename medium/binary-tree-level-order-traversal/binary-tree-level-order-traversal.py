5c902d00a4c4fed065c034d7ca62e71ba23a548e
jibrilfields <105961737+jibrilfields@users.noreply.github.com>
2022-05-22 23:42:11 +0300

    Create binary-tree-level-order-traversal.py

---
+++ medium/binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
@@ -0,0 +1,12 @@
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        def bfs(tree, resultant, level):
+            if tree != None:
+                if len(resultant) <= level:
+                    resultant.append([])
+                resultant[level].append(tree.val)
+                bfs(tree.left, resultant, level+1)
+                bfs(tree.right, resultant, level+1)
+        result = []
+        bfs(root, result, 0)
+        return result
