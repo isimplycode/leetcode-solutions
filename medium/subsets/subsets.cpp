0879d694ef92d1116abfb22cc515555c73d9f394
jibrilfields <105961737+jibrilfields@users.noreply.github.com>
2022-06-22 05:47:48 +0300

    Create subsets.cpp

---
+++ medium/subsets/subsets.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> res;
+    vector<int> subset;
+    void gen(int n, vector<int> nums) {
+        if (n==size(nums)) {
+            vector<int> append(subset);
+            res.push_back(append);
+            return;
+        }
+        subset.push_back(nums[n]);
+        gen(n+1,nums);
+        subset.pop_back();
+        gen(n+1,nums);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        gen(0,nums);
+        return res;
+    }
+};
