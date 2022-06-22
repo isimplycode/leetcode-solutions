45f88543a9d5ff84e1f5ce51330a3870727ee203
jibrilfields <105961737+jibrilfields@users.noreply.github.com>
2022-06-23 01:50:47 +0300

    Create climbing-stairs.cpp

---
+++ easy/climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int count[46];
+    int climbStairs(int n) {
+        count[0]=1;
+        count[1]=1;
+        for (int i=2; i<=n; i++) {
+            count[i]=count[i-1]+count[i-2];
+        }
+        return count[n];
+    }
+};
